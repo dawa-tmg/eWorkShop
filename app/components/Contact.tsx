3b4189850446b9bae2fdf105ba10c366e5f7c088
dawa.t <dawadorji295@gmail.com>
2025-08-05 10:19:06 +0600

    Adds About-Us page, customized Footer and Contact Page

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,11 +1,44 @@
+import Input from "./Input"
+import Button from "./Button"
 export default function ContactForm(){
     return (
-        <div>
-            <form className="w-[40%] border-2 rounded-xl grid grid-cols-1 gap-5 p-5 mx-auto">
-                <input className="focus:outline-none border-2 rounded-lg p-2" type="email" name="mail" placeholder="Email" />
-                <textarea className="focus:outline-none border-2 rounded-lg p-2" name="message" minLength={200} rows={10} placeholder="Write Your Message Here"></textarea>
-                <button className="border-2 rounded-lg p-2" type="submit">Submit</button>
-            </form>
-        </div>
+        <form className="bg-[#eee] w-[70%] shadow-lg grid grid-cols-1 gap-5 p-10 mx-auto">
+            <h2 className="text-4xl text-red-500 font-bold pb-5">Online Estimate Form</h2>
+            <Input 
+            type = 'text'
+            name = 'name'
+            placeholder = 'Your Name'
+            />
+            <Input 
+            type = 'email'
+            name = 'mail'
+            placeholder = 'Email Address'
+            />
+            <Input 
+            type = 'number'
+            name = 'phone'
+            placeholder = 'Phone Number'
+            />
+            <select className="bg-none focus:outline-none border border-gray-400 rounded text-gray-500 px-2 py-4" name="" id="">
+                <option value="">Select Service</option>
+                <option value="Repair">Repair</option>
+                <option value="Tyre Change">Tyre Change</option>
+                <option value="Wheel Alignment">wheel Alignment</option>
+            </select>
+            <Input 
+            type = 'text'
+            name = 'model'
+            placeholder = 'Year Make & Model of Vehicle'
+            />
+            <Input 
+            type = 'date'
+            name = 'date'
+            placeholder = ''
+            />
+            <textarea className="focus:outline-none border border-gray-400 rounded-lg p-2" name="message" minLength={200} rows={4} placeholder="Describe Issue and Service Request"></textarea>
+            <Button 
+            text = 'Submit Now'
+            />
+        </form>
     )
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,36 @@ import ContactForm from "../components/Contact"
 
 export default function Contact(){
     return (
-        <div>
+        <div className="grid grid-cols-2 gap-5">
+            <div>
+                <h1 className="text-5xl text-gray-500 font-bold py-10">Why Choose eWorkShop</h1>
+                <ul>
+                    <li className="text-xl flex py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 w-[10%]">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        <p className="w-[90%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, voluptatum.</p>
+                    </li>
+                    <li className="text-xl flex py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 w-[10%]">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        <p className="w-[90%]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptates labore itaque ratione repellendus omnis enim corporis cumque aliquid illo.</p>
+                    </li>
+                    <li className="text-xl flex py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 w-[10%]">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        <p className="w-[90%]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed placeat corporis accusantium ducimus. Enim et, quisquam magni fugit cum, deserunt exercitationem unde natus molestiae id delectus aliquid saepe dolorem possimus!</p>
+                    </li>
+                    <li className="text-xl flex py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 w-[10%]">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        <p className="w-[90%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quae accusamus molestiae sapiente inventore voluptatum quam optio, laboriosam enim accusantium debitis culpa, molestias sequi dicta adipisci nam. Voluptatum odit vitae nostrum dolor voluptatem omnis repudiandae tempora quod iste corporis. Iusto!</p>
+                    </li>
+                </ul>
+            </div>
             <ContactForm />
         </div>
     )
